9003b3cae268f80d8e6051782f4e23c323e24130
zeozdev <zeoz@nysr.host>
2024-09-25 00:33:56 -0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,88 @@
+from flask import Flask, render_template, redirect, url_for, request, session
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+import re
+
+app = Flask(__name__)
+
+# Credenciales de la aplicación de Spotify
+client_id = "84a3895b12b5431da7c325c75d7cebbe"
+client_secret = "09be01d08b7c4952bf539ff356f7d48f"
+redirect_uri = "http://127.0.0.1:5000/callback" 
+
+# Configura la autorización de Spotify
+scopes = "user-read-private user-read-email playlist-read-private playlist-modify-public user-library-modify"
+sp_oauth = SpotifyOAuth(
+    client_id=client_id,
+    client_secret=client_secret,
+    redirect_uri=redirect_uri,
+    scope=scopes
+)
+
+# Define la ruta para el inicio de sesión
+@app.route("/")
+def index():
+    # Verifica si el usuario ya ha iniciado sesión
+    if "spotify_token" in session:
+        return render_template("index.html")
+    else:
+        auth_url = sp_oauth.get_authorize_url()
+        return render_template("login.html", auth_url=auth_url)
+
+# Define la ruta para el callback de la autenticación
+@app.route("/callback")
+def callback():
+    code = request.args.get("code")
+    token_info = sp_oauth.get_access_token(code)
+    session["spotify_token"] = token_info['access_token']
+    return redirect(url_for("index"))
+
+# Define la ruta para obtener recomendaciones
+@app.route("/recomendaciones", methods=["POST"])
+def recomendaciones():
+    # Obtén el token de acceso del usuario
+    token = session.get("spotify_token")
+    if token:
+        sp = spotipy.Spotify(auth=token)
+        spotify_link = request.form.get("spotify_link")
+        if spotify_link:
+            # Extrae el ID de la canción del enlace de Spotify
+            match = re.search(r'track/([a-zA-Z0-9]+)', spotify_link)
+            if match:
+                track_id = match.group(1)
+                recomendaciones = sp.recommendations(seed_tracks=[track_id], limit=5)
+                recommendations_list = recomendaciones["tracks"]
+                print("Recomendaciones:", recommendations_list)  # Imprime la lista en la consola
+                return render_template("recomendaciones.html", recomendaciones=recommendations_list)
+            else:
+                return "Enlace de Spotify inválido"
+        else:
+            return "Falta enlace de Spotify"
+    else:
+        return "Debes iniciar sesión"
+
+# Función para guardar/eliminar "Me gusta"
+@app.route('/like', methods=['POST'])
+def like_track():
+    token = session.get("spotify_token")
+    if token:
+        sp = spotipy.Spotify(auth=token)
+        track_id = request.form.get("track_id")
+        is_liked = request.form.get("liked") == 'true'
+
+        if is_liked:
+            # Eliminar de "Me gusta"
+            sp.current_user_saved_tracks_delete(tracks=[track_id])
+            return "Eliminado de 'Me gusta'"
+        else:
+            # Guardar en "Me gusta"
+            sp.current_user_saved_tracks_add(tracks=[track_id])
+            return "Agregado a 'Me gusta'"
+    else:
+        return "Debes iniciar sesión"
+
+# Configura la aplicación para que use sesiones
+app.secret_key = "your_secret_key"
+
+if __name__ == "__main__":
+    app.run(debug=True)
